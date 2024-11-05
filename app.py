a4f5eddbcef1f05ddbd4b0148ec1c5b1b70e85c8
Juan Pablo Gallego <jpgallegoar@users.noreply.huggingface.co>
2024-11-05 13:16:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import subprocess
 
 def install_package():
+    subprocess.run(["pip", "uninstall", "-y", "Spanish-F5"], check=True)
     subprocess.run(["pip", "install", "--upgrade", "git+https://github.com/jpgallegoar/Spanish-F5", "--no-cache-dir"], check=True)
-
 
 # Install the package on startup
 install_package()
 
 # Start the Gradio app
 subprocess.run(["f5-tts_infer-gradio"])
-
